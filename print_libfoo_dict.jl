8779181cf00fbf0648dbec42e92751937574efed
Elliot Saba <staticfloat@gmail.com>
2017-09-02 23:09:18 -0700

    Better printing

--- print_libfoo_dict.jl
+++ print_libfoo_dict.jl
@@ -3,9 +3,10 @@
 using SHA
 using BinaryProvider
 
-tarballs = [f for f in readdir(".") if startswith(f, "libfoo") && endswith(f, ".tar.gz")]
-for filename in tarballs
-    triplet = filename[8:end-7]
+tarballs = [f for f in readdir(".") if startswith(f, "libfoo") &&  endswith(f, ".tar.gz")]
+mapping = Dict(platform_key(f[8:end-7]) => f for f in tarballs)
+for target in sort(collect(keys(mapping)))
+    filename = mapping[target]
     hash = bytes2hex(sha256(open(filename)))
-    println("    \"$(platform_key(triplet))\" => (\"\$prefix/$(filename)\", \"$(hash)\"),")
+    println("    :$(target) => (\"\$bin_prefix/$(filename)\", \"$(hash)\"),")
 end
