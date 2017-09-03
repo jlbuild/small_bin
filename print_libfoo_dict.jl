2d18c888995b4118bdbed9668e0b19f82dd7965c
Elliot Saba <staticfloat@gmail.com>
2017-09-02 22:57:34 -0700

    Better printing

--- print_libfoo_dict.jl
+++ print_libfoo_dict.jl
@@ -4,5 +4,8 @@ using SHA
 using BinaryProvider
 
 tarballs = [f for f in readdir(".") if startswith(f, "libfoo") && endswith(f, ".tar.gz")]
-libfoo_downloads = Dict(platform_key(f[8:end-7]) => ("\$small_bin_prefix/$f", bytes2hex(sha256(open(f)))) for f in tarballs)
-@show libfoo_downloads
+for filename in tarballs
+    triplet = filename[8:end-7]
+    hash = bytes2hex(sha256(open(filename)))
+    println("    \"$(platform_key(triplet))\" => (\"\$prefix/$(filename)\", \"$(hash)\"),")
+end
