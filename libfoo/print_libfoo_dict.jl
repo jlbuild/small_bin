51b13b44feb2a262e2e04690bfa54d03167533f2
staticfloat@gmail.com <staticfloat@gmail.com>
2018-06-04 13:12:42 -0700

    Move `libfoo` to its own folder

--- libfoo/print_libfoo_dict.jl
+++ libfoo/print_libfoo_dict.jl
@@ -5,8 +5,8 @@ using BinaryProvider
 
 tarballs = [f for f in readdir(".") if startswith(f, "libfoo") &&  endswith(f, ".tar.gz")]
 mapping = Dict(platform_key(f[8:end-7]) => f for f in tarballs)
-for target in sort(collect(keys(mapping)))
+for target in sort(collect(keys(mapping)), by = k -> triplet(k))
     filename = mapping[target]
     hash = bytes2hex(sha256(open(filename)))
-    println("    :$(target) => (\"\$bin_prefix/$(filename)\", \"$(hash)\"),")
+    println("    $(repr(target)) => (\"\$bin_prefix/$filename\", $(repr(hash))),")
 end
